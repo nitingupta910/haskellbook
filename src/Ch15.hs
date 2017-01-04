1a4b84cfc538e89d18ca8ee6e19b5b385c521b4d
Nitin Gupta <nitin.m.gupta@oracle.com>
2017-01-04 12:10:40 -0800

    Initial commit for Ch15

--- src/Ch15.hs
+++ src/Ch15.hs
@@ -1,2 +1,15 @@
 module Ch15 where
 
+-- CHAPTER 15. MONOID, SEMIGROUP
+
+data Optional a =
+    Nada
+  | Only a
+  deriving (Eq, Show)
+
+instance Monoid a => Monoid (Optional a) where
+  mempty = Nada
+  mappend Nada Nada = Nada
+  mappend Nada (Only a) = Only a
+  mappend (Only a) Nada = Only a
+  mappend (Only a) (Only a') = Only (mappend a a')
