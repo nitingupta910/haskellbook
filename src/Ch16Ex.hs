f736f7126587f0f2075981b8640f600cfcf14a7d
Nitin Gupta <nitingupta910@gmail.com>
2017-01-17 09:27:34 -0800

    ch16: last question left

--- src/Ch16Ex.hs
+++ src/Ch16Ex.hs
@@ -65,6 +65,7 @@ instance Functor (More x) where
   fmap f (L a b a') = L (f a) b (f a')
   fmap f (R b a b') = R b (f a) b'
 
+-- 1
 data Quant a b
   = Finance
   | Desk a
@@ -75,18 +76,87 @@ instance Functor (Quant a) where
   fmap _ (Desk a) = Desk a
   fmap f (Bloor b) = Bloor (f b)
 
-data K' a b =
-  K' a
+-- 2
+newtype K a b =
+  K a
 
-instance Functor (K' a) where
-  fmap f (K' a) = K' a
+instance Functor (K a) where
+  fmap f (K a) = K a
 
+-- 3
 newtype Flip f a b =
   Flip (f b a)
   deriving (Eq, Show)
 
-newtype K a b =
-  K a  
-
 instance Functor (Flip K a) where
-  fmap f (Flip f' b a) = Flip 
+  fmap f (Flip (K b)) = Flip (K (f b))
+
+-- 4
+data EvilGoateeConst a b =
+  GoatyConst b
+
+instance Functor (EvilGoateeConst a) where
+  fmap f (GoatyConst b) = GoatyConst (f b)
+
+-- 5
+data LiftItOut f a  =
+  LiftItOut (f a)
+
+instance Functor f => Functor (LiftItOut f) where
+  fmap f (LiftItOut t) = LiftItOut (fmap f t)
+
+-- 6
+data Parappa f g a =
+  DaWrappa (f a) (g a)
+
+instance (Functor f, Functor g) => Functor (Parappa f g) where
+  fmap f (DaWrappa k k') = DaWrappa (fmap f k) (fmap f k')
+
+-- 7
+data IgnoreOne f g a b =
+  IgnoringSomething (f a) (g b)
+
+instance Functor g => Functor (IgnoreOne f g a) where
+  fmap f (IgnoringSomething k k') = IgnoringSomething k (fmap f k')
+
+-- 8
+data Notorious g o a t =
+  Notorious (g o) (g a) (g t)
+
+instance Functor g => Functor (Notorious g o a) where
+  fmap f (Notorious k k' k'') = Notorious k k' (fmap f k'')
+
+-- 9
+data List a
+  = Nil
+  | Cons a
+         (List a)
+
+instance Functor List where
+  fmap f Nil = Nil
+  fmap f (Cons a k) = (Cons $ f a) (fmap f k)
+
+-- 10
+data GoatLord a
+  = NoGoat
+  | OneGoat a
+  | MoreGoat (GoatLord a)
+             (GoatLord a)
+             (GoatLord a)
+
+instance Functor GoatLord where
+  fmap f NoGoat = NoGoat
+  fmap f (OneGoat a) = OneGoat $ f a
+  fmap f (MoreGoat k k' k'') = MoreGoat (fmap f k) (fmap f k') (fmap f k'')
+
+-- 11
+data TalkToMe a
+  = Halt
+  | Print String
+          a
+  | Read (String -> a)
+
+instance Functor TalkToMe where
+  fmap f Halt = Halt
+  fmap f (Print s a) = Print s (f a)
+  --fmap f (Read f') = Read f
